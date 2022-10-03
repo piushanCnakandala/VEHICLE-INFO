14e07811f6d0fffc258f81f0fb91c924ff731fa2
piushanCnakandala <piushancnakandala2000@gmail.com>
2022-10-03 22:02:58 +0530

    npm install react-navigation/stack,gesture-handler

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import Login from './screens/Login'
+import { NavigationContainer } from '@react-navigation/native';
 
 
 
 export default function App() {
   return (
-   <Login/>
+    <NavigationContainer>
+       <Login/>
+    </NavigationContainer>
+  
   )
 }

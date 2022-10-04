95d19f4fdd65896609b7b0f4e0f753387945acb2
piushanCnakandala <piushancnakandala2000@gmail.com>
2022-10-04 21:35:48 +0530

    use Flatlist for  Home page

--- screens/Home.js
+++ screens/Home.js
@@ -1,10 +1,35 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text,StyleSheet,FlatList,TouchableOpacity } from 'react-native'
+import {React,useEffect,useState}  from  'react'
 
 export default function Home() {
+
+    const [posts,setPosts] =useState([]);
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then((response) => response.json())
+            .then((json) => setPosts(json));
+    })
+
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+       <FlatList
+                data={posts}
+                renderItem={({ item }) =>
+                    <TouchableOpacity style={{borderWidth:1,backgroundColor:indigo, marginBottom:'5%', padding:5}} onPress={()=>{console.log("hello");}}>
+                        <Text style={{marginBottom:10,fontWeight:'bold'}} >{item.title}</Text>
+                        <Text style={{marginBottom:10}} >{item.body}</Text>
+                    </TouchableOpacity>
+                }
+            />
+      
     </View>
   )
 }
+const styles=StyleSheet.create({
+    container:{
+        flex:1,
+        padding:20
+    }
+
+})

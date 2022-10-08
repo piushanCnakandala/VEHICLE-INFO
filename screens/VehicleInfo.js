ae869c59ff06e0911262668b26f90cd31103cd46
piushanCnakandala <piushancnakandala2000@gmail.com>
2022-10-08 13:42:58 +0530

    backend update

--- screens/VehicleInfo.js
+++ screens/VehicleInfo.js
@@ -8,7 +8,7 @@ export default function VehicleInfo({navigation}) {
     <View style={styles.container}>
       <NativeBaseProvider>
       <Fab onPress={()=>{navigation.navigate("VehicleInfo")}} renderInPortal={false} shadow={2} bottom={90}  size="sm" icon={<Ionicons color="white" name={"create-outline"} size={18} onPress={()=>{navigation.navigate("Update")}}/>} />
-      <Fab onPress={()=>{navigation.navigate("VehicleInfo")}} renderInPortal={false} shadow={2}  size="sm" icon={<Ionicons color="white" name={"trash-outline"} size={18} />} />
+      <Fab onPress={()=>{navigation.navigate("VehicleInfo")}} renderInPortal={false} shadow={2}  size="sm" icon={<Ionicons color="white" name={"trash-outline"} size={18} onPress={()=>{navigation.navigate("Home")}} />} />
       
 
      <VStack space={4} alignItems="center" mt="10%">

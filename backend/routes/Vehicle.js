80c7bf947b64a0acfff53a5ff1784136d019c47f
piushanCnakandala <piushancnakandala2000@gmail.com>
2022-10-08 18:24:21 +0530

    Vehicle post  done

--- backend/routes/Vehicle.js
+++ backend/routes/Vehicle.js
@@ -11,3 +11,18 @@ router.get('/', async (req, res) => {
         res.send('error :' + e)
     }
 })
+router.post('/', async (req, res) => {
+    const cars = new Car({
+        regNo: req.body.regNo,
+        description: req.body.description,
+        brandName: req.body.brandName
+    })
+    try {
+        const response = await cars.save();
+        res.json(response)
+    } catch (e) {
+        res.send('error :' + e)
+    }
+})
+
+
